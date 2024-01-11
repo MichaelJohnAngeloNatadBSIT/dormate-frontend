2c2d13677c6b01913273467dde65f9e08575e8b4
MichaelJohnAngeloNatadBSIT <201911707@gordoncollege.edu.ph>
2024-01-11 19:31:01 +0800

    disabled socket

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -59,15 +59,15 @@ export class ProfileComponent implements OnInit {
     this.currentUser = this.tokenService.getUser();
     this.retrieveUser();
     this.retrieveForApprovalDorm();
-    this.chatService.setupSocketConnection();
+    // this.chatService.setupSocketConnection();
     this.retrieveForApprovalScheduleLandlord();
     this.retrieveForApprovalScheduleTenant();
     this.retrieveForApprovalScheduleApprovedLandlord()
     this.retrieveForApprovalScheduleApprovedTenant()
   }
-  ngOnDestroy() {
-    this.chatService.disconnect();
-  }
+  // ngOnDestroy() {
+  //   this.chatService.disconnect();
+  // }
 
   calendarOptions: CalendarOptions = {
     plugins: [dayGridPlugin, timeGrigPlugin, interactionPlugin],
