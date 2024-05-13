312674ef73c0b5d8c06fd69166df03acd0c70f02
MichaelJohnAngeloNatadBSIT <201911707@gordoncollege.edu.ph>
2024-05-13 17:03:57 +0800

    added updating of data after evicting in dorm

--- src/app/dialogs/evict-as-tenant-dialog/evict-as-tenant-dialog.component.ts
+++ src/app/dialogs/evict-as-tenant-dialog/evict-as-tenant-dialog.component.ts
@@ -2,6 +2,7 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog';
 import { Dorm } from 'src/app/models/dorms.model';
 import { DormService } from 'src/app/services/dorm.service';
+import { UserService } from 'src/app/services/user.service';
 
 interface Tenant {
   tenant_username?: string;
@@ -22,21 +23,39 @@ export class EvictAsTenantDialogComponent implements OnInit {
 
   tenant: Tenant;
   tenant_user_id: any;
+  user_evict_data: any;
   
   constructor(
     public dialogRef: MatDialogRef<EvictAsTenantDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any,
     private dormService: DormService,
+    private userService: UserService,
   ){
 
   }
 
   ngOnInit(): void {
     this.tenant = this.data.tenant;
+    console.log(this.data);
   }
   
   evictAsTenant(){
     this.dormService.evictTenant(this.data.dorm._id, this.data.tenant.tenant_user_id).subscribe({
+      next: (res) => {
+
+      },
+      error: (e) => console.error(e)
+    });
+
+    this.user_evict_data = {
+      dorm_id: "",
+      dorm_title: "",
+      dorm_landlord_user_id: "",
+      is_tenant: false,
+      dorm_tenant_date: "" 
+    };
+
+    this.userService.updateUser(this.data.tenant.tenant_user_id, this.user_evict_data).subscribe({
       next: (res) => {
 
       },
