dbc7b5ca08e0e3b417c0242d6939d9210b054df9
MichaelJohnAngeloNatadBSIT <201911707@gordoncollege.edu.ph>
2024-06-10 07:08:17 +0800

    added tenant cant schedule visit if not verified

--- src/app/components/dorm/dorm.component.ts
+++ src/app/components/dorm/dorm.component.ts
@@ -11,6 +11,7 @@ import { InfoScheduleDialogComponent } from 'src/app/dialogs/info-schedule-dialo
 import { User } from 'src/app/interface/user';
 import { Review } from 'src/app/interface/review';
 import { UserService } from 'src/app/services/user.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 
 @Component({
@@ -30,7 +31,8 @@ reviews: Review[];
                 private userService: UserService,
                 private dialog: MatDialog,
                 private tokenService: TokenStorageService,
-                private router: Router
+                private router: Router,
+                private snackBar: MatSnackBar,
             ) {}
     ngOnInit() {
       this.dorm_id = this.route.snapshot.paramMap.get('id');
@@ -61,6 +63,7 @@ reviews: Review[];
 
     
     openInfoSchedDialog(dorm: Dorm){
+      if(this.currentUser.verified == true){
       var isAuthenticated = this.tokenService.isLoggedIn();
       if (!isAuthenticated) { 
         this.router.navigate(['/login']); 
@@ -73,8 +76,21 @@ reviews: Review[];
         data: {user: this.currentUser, dorm: dorm}
       }); 
     }
-
+    } else{
+      this.showSnackbarTopPosition('Please Make Your account Verified by Uploading a Valid ID','Ok', '10000')
+      this.router.navigate(['/profile']);
     }
+
+  }
+
+  showSnackbarTopPosition(content, action, duration) {
+    this.snackBar.open(content, action, {
+      duration: duration,
+      verticalPosition: "top", // Allowed values are  'top' | 'bottom'
+      horizontalPosition: "center", // Allowed values are 'start' | 'center' | 'end' | 'left' | 'right'
+      panelClass: ['custom-style']
+    });
+  }
 
 
     config: SwiperOptions = {
